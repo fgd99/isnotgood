38613dad44d731ec035ffaf0f3c5255a9366e0c3
Fabrice <fabriceguydominguez@gmail.com>
2015-11-04 19:47:43 +0100

    Changement de nom de domaine

--- index.js
+++ index.js
@@ -78,10 +78,9 @@ if (require.main === module) {
   var templateFilename = path.join(__dirname, 'template.jade'),
       compiledTemplate = jade.compileFile(templateFilename, { pretty: true, filename: templateFilename });
 
-  var urlSeries = 'http://www.cpasbien.pw/view_cat.php?categorie=series',
-      urlFilms = 'http://www.cpasbien.pw/view_cat.php?categorie=films';
+  var urlSeries = 'http://www.cpasbien.io/view_cat.php?categorie=series',
+      urlFilms = 'http://www.cpasbien.io/view_cat.php?categorie=films';
 
   generateOutputFor(compiledTemplate, urlSeries, 'series.html');
   generateOutputFor(compiledTemplate, urlFilms, 'films.html');
 }
-
